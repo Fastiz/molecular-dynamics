e4f68ffb74293985fbc851d79b86c2c47d450c6c
fastiz <fastiz@itba.edu.ar>
2020-04-13 12:48:30 -0300

    graphs for difference between analytical and numerical solutions. Also calculates the mean squared error.

--- python_graphs/main.py
+++ python_graphs/main.py
@@ -3,22 +3,37 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    plot_positions()
+    plot_numerical_vs_analytical()
 
 
-def plot_positions():
+def plot_numerical_vs_analytical():
+    gamma = 1000
+    k = 10000
+    mass = 70
+    step = 0.001
+
     filename = "../output"
 
-    values = []
+    numerical_solution = []
     with open(filename, 'r') as f:  # open in readonly mode
         lines = [line.rstrip() for line in f]
         for line in lines:
-            values.append(float(line))
+            numerical_solution.append(float(line))
 
-    times = np.arange(0, len(values), 1)
+    times = np.arange(0, len(numerical_solution)*step, step)
 
-    plt.plot(times, values)
+    # Solution for overdamped oscillator
+    analytical_solution = np.exp(-(gamma/(2*mass))*times)*np.cos(np.sqrt(k/mass - (gamma**2)/(4*(mass**2)))*times)
+
+    plt.plot(times, analytical_solution)
+    plt.plot(times, numerical_solution)
+
     plt.show()
+
+    errors = [((numerical-analytical)**2) for numerical, analytical in zip(numerical_solution, analytical_solution)]
+    mean_squared_error = sum(errors)/len(times)
+    
+    print("Error cuadratico medio: %s" % mean_squared_error)
 
 
 if __name__ == "__main__":
