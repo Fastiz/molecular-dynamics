8e9bd74aed2e78a88abdd9c2bd748030b0628766
Miguel <mdiluca@itba.edu.ar>
2020-04-24 02:09:45 -0300

    stuff

--- src/CelestialBodiesSimulation.java
+++ src/CelestialBodiesSimulation.java
@@ -9,7 +9,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CelestialBodiesSimulation {
     private static final double JUPITER_X = 1.8423672321997e8;
@@ -59,27 +61,27 @@ public class CelestialBodiesSimulation {
     private static final double SPACESHIP_DISTANCE = EARTH_DISTANCE + 1500 + 6371.01;
     private static final double SPACESHIP_X = SPACESHIP_DISTANCE * Math.cos(EARTH_ANGLE);
     private static final double SPACESHIP_Y = SPACESHIP_DISTANCE * Math.sin(EARTH_ANGLE);
-    private static final double SPACESHIP_V = 8 + 7.12;
+    private static final double SPACESHIP_V = 8.0 + 7.12;
     private static final double SPACESHIP_VX = SPACESHIP_V * Math.cos(EARTH_V_ANGLE);
     private static final double SPACESHIP_VY = SPACESHIP_V * Math.sin(EARTH_V_ANGLE);
 
     public void runAnimation() throws IOException {
         List<Particle> particlesList = getPlanetList();
 
-        int step = 50;
+        double step = 50;
         SimpleBeeman algorithm = new SimpleBeeman(particlesList, new GravityModel(), step);
 
 
         double maxMagnitude = 0;
         try(BufferedWriter bf = new BufferedWriter(new FileWriter("dynamic_file"))){
             boolean flag = false;
-            for(long t=0; t< 400000; t++){
-                if (!flag && t >= 8453965 / step) {
+            for(long t=0; t< 8467200 * 16 / step; t++){
+                if (!flag && t >= 8467200 / step) {
                     flag = true;
                     Particle earth = algorithm.getParticles().get(1);
                     algorithm.addParticle(generateSpaceship(earth));
                 }
-                if(t % 1200 == 0) {
+                if(t % 100 == 0) {
                     bf.write("#T" + t + "\n");
                     List<Particle> current = algorithm.getParticles();
                     for (Particle particle : current) {
@@ -109,38 +111,45 @@ public class CelestialBodiesSimulation {
         List<Particle> particlesList = getPlanetList();
 
 
+        (new File("results/")).mkdir();
         (new File("results/gravity/")).mkdir();
 
         Utils.deleteFiles("results/gravity/");
 
         int step = 50;
         int TIME_STEP = 24 * 60 * 60 / step;
-        int TIME_ITERATIONS = 150;
+        int TIME_ITERATIONS = 700;
 
-        for(int it=1; it<TIME_ITERATIONS+1; it++){
+        for(int it=0; it<=TIME_ITERATIONS; it++){
             System.out.println("Simulating launch with offset: " + TIME_STEP*it*step +" s and index: " + it +".");
 
             try(BufferedWriter bf = new BufferedWriter(new FileWriter("results/gravity/sim"+it))){
-                Beeman algorithm = new Beeman(new ArrayList<>(particlesList), new GravityModel(), step);
-
-                Vector minDistance = new Vector(Double.MAX_VALUE, Double.MAX_VALUE);
-                boolean flag = false;
                 bf.write((int)Math.ceil(it * TIME_STEP / 1000.0) + "\n");
-                for(long t=0; t<300000 * 73 / step; t++){
-                    if(!flag && t >= it*TIME_STEP){
-                        flag = true;
+
+                Beeman algorithm = new Beeman(new ArrayList<>(particlesList), new GravityModel(), step);
+                Vector minDistance = new Vector(Double.MAX_VALUE, Double.MAX_VALUE);
+                boolean spaceshipTookOff = false;
+                boolean alreadyPassedMars = false;
+
+                for(long t=0; !alreadyPassedMars; t++){
+                    if(!spaceshipTookOff && t >= it*TIME_STEP){
+                        spaceshipTookOff = true;
                         Particle earth = algorithm.getParticles().get(1);
                         algorithm.addParticle(generateSpaceship(earth));
                     }
 
-                    if (flag) {
-                        Vector marsPos = algorithm.getParticles().get(2).getPos();
-                        Vector spaceshipPos = algorithm.getParticles().get(3).getPos();
+                    if (spaceshipTookOff) {
+                        List<Particle> particleList = algorithm.getParticles();
+                        Vector marsPos = particleList.get(2).getPos();
+                        Vector spaceshipPos = particleList.get(particleList.size()-1).getPos();
                         double distanceModule = Math.sqrt(Math.pow(spaceshipPos.getX() - marsPos.getX(), 2) + Math.pow(spaceshipPos.getY() - marsPos.getY(), 2));
                         if (distanceModule < minDistance.getModule()) {
                             minDistance.setX(spaceshipPos.getX() - marsPos.getX());
                             minDistance.setY(spaceshipPos.getY() - marsPos.getY());
                         }
+
+                        if(marsPos.magnitude() * 1.25 < spaceshipPos.magnitude())
+                            alreadyPassedMars = true;
                     }
 
                     if(t % 1000 == 0) {
@@ -157,10 +166,9 @@ public class CelestialBodiesSimulation {
 
                     algorithm.step();
                 }
-                System.out.println("Min: " + minDistance.getModule());
+                System.out.println("Min Distance: " + minDistance.magnitude());
             }
         }
-
     }
 
     public void runAlternativeSystemAnimation() throws IOException{
@@ -173,12 +181,8 @@ public class CelestialBodiesSimulation {
         double maxMagnitude = 0;
         try(BufferedWriter bf = new BufferedWriter(new FileWriter("dynamic_file"))){
             boolean flag = false;
-            for(long t=0; t< 800000; t++){
-                if (!flag && t >= 98 * 24 * 60 * 60 / step) {
-                    flag = true;
-                    Particle earth = algorithm.getParticles().get(1);
-                    algorithm.addParticle(generateSpaceship(earth));
-                }
+            for(long t=0; t< 12000000; t++){
+
                 if(t % 600 == 0) {
                     bf.write("#T" + t + "\n");
                     List<Particle> current = algorithm.getParticles();
@@ -186,9 +190,7 @@ public class CelestialBodiesSimulation {
                         Vector pos = particle.getPos();
 
                         double magnitude = pos.magnitude();
-
                         maxMagnitude = Math.max(magnitude, maxMagnitude);
-
                         bf.write(pos.getX() + " " + pos.getY() + "\n");
                     }
                 }
@@ -205,7 +207,6 @@ public class CelestialBodiesSimulation {
     }
 
     public void searchLowerTravelTime(int seconds) throws IOException {
-        seconds = 203100 * 60;
 
         List<Particle> particlesList = getPlanetList();
 
@@ -213,34 +214,23 @@ public class CelestialBodiesSimulation {
         (new File("results/velocity/")).mkdir();
 
         Utils.deleteFiles("results/velocity/");
+        Map<Double, Double> map = new HashMap<>();
+        double velocityStep = 0.01;
+        double step = 0.1;
 
-        double velocityStep = 0.0000001;
-        int step = 73;
-
-        int globalMinTime = Integer.MAX_VALUE;
         try (BufferedWriter bf = new BufferedWriter(new FileWriter("results/velocity/timePerVelocity"))) {
-            boolean meetCondition = true;
-            for (double spaceshipVelocity = SPACESHIP_V; meetCondition; spaceshipVelocity += velocityStep) {
+            for (double spaceshipVelocity = SPACESHIP_V - 0.01; spaceshipVelocity < 8.01 + 7.12; spaceshipVelocity += velocityStep) {
 
                 SimpleBeeman algorithm = new SimpleBeeman(new ArrayList<>(particlesList), new GravityModel(), step);
 
-                Vector minDistance = new Vector(Double.MAX_VALUE, Double.MAX_VALUE);
                 boolean flag = false;
-                int minTime = 0;
-                for (int t = 0; t < 400000; t++) {
+                boolean cutSimulation = false;
+                System.out.println("Simulation with speed: " + spaceshipVelocity);
+                for (int t = 0; !cutSimulation; t++) {
                     if (!flag && t >= seconds / step) {
                         flag = true;
                         Particle earth = algorithm.getParticles().get(1);
-
-                        double EARTH_V_ANGLE = Math.atan(earth.getVel().getY() / earth.getVel().getX());
-                        double EARTH_ANGLE = Math.atan2(earth.getPos().getY(), earth.getPos().getX());
-                        double SPACESHIP_DISTANCE = earth.getPos().magnitude() + 1500;
-                        double SPACESHIP_X = SPACESHIP_DISTANCE * Math.cos(EARTH_ANGLE);
-                        double SPACESHIP_Y = SPACESHIP_DISTANCE * Math.sin(EARTH_ANGLE);
-                        double SPACESHIP_VX = spaceshipVelocity * Math.cos(EARTH_V_ANGLE);
-                        double SPACESHIP_VY = spaceshipVelocity * Math.sin(EARTH_V_ANGLE);
-
-                        algorithm.addParticle(new Particle(new Vector(SPACESHIP_X, SPACESHIP_Y), new Vector(SPACESHIP_VX, SPACESHIP_VY), 30, SPACESHIP_MASS));
+                        algorithm.addParticle(generateSpaceship(earth, spaceshipVelocity));
                     }
 
                     algorithm.step();
@@ -249,27 +239,17 @@ public class CelestialBodiesSimulation {
                         Vector marsPos = algorithm.getParticles().get(2).getPos();
                         Vector spaceshipPos = algorithm.getParticles().get(3).getPos();
                         double distanceModule = Math.sqrt(Math.pow(spaceshipPos.getX() - marsPos.getX(), 2) + Math.pow(spaceshipPos.getY() - marsPos.getY(), 2));
-                        if (distanceModule < minDistance.getModule()) {
-                            minDistance.setX(spaceshipPos.getX() - marsPos.getX());
-                            minDistance.setY(spaceshipPos.getY() - marsPos.getY());
-                            minTime = t * step;
+                        if (distanceModule < MARS_RATIO * 3) {
+                            map.put(spaceshipVelocity, t * step);
+                            cutSimulation = true;
+                        } else if (spaceshipPos.magnitude() > marsPos.magnitude() * 1.5) {
+                            cutSimulation = true;
                         }
-                        if(minTime < globalMinTime)
-                            globalMinTime = minTime;
                     }
                 }
-
-                meetCondition = meetCondition(minDistance);
-                if(meetCondition) {
-                    bf.write(minTime + " " + spaceshipVelocity);
-                }
             }
-            System.out.println(globalMinTime);
+            System.out.println(map);
         }
-    }
-
-    private boolean meetCondition(Vector minDistance) {
-        return minDistance.magnitude() < MARS_RATIO;
     }
 
     private Particle generateSpaceship(Particle earth) {
@@ -284,10 +264,22 @@ public class CelestialBodiesSimulation {
         return new Particle(new Vector(SPACESHIP_X, SPACESHIP_Y), new Vector(SPACESHIP_VX, SPACESHIP_VY), 30, SPACESHIP_MASS);
     }
 
+    private Particle generateSpaceship(Particle earth, double spaceshipVel) {
+        double EARTH_V_ANGLE = Math.atan2(earth.getVel().getY(), earth.getVel().getX());
+        double EARTH_ANGLE = Math.atan2(earth.getPos().getY(), earth.getPos().getX());
+        double SPACESHIP_DISTANCE = earth.getPos().magnitude() + 1500 + 6371.01;
+        double SPACESHIP_X = SPACESHIP_DISTANCE * Math.cos(EARTH_ANGLE);
+        double SPACESHIP_Y = SPACESHIP_DISTANCE * Math.sin(EARTH_ANGLE);
+        double SPACESHIP_VX = (spaceshipVel * Math.cos(EARTH_V_ANGLE)) + (-Math.sin(EARTH_ANGLE) * earth.getVel().magnitude());
+        double SPACESHIP_VY = (spaceshipVel * Math.sin(EARTH_V_ANGLE)) + (Math.cos(EARTH_ANGLE) * earth.getVel().magnitude());
+
+        return new Particle(new Vector(SPACESHIP_X, SPACESHIP_Y), new Vector(SPACESHIP_VX, SPACESHIP_VY), 30, SPACESHIP_MASS);
+    }
+
     private List<Particle> getAlternativePlanetsList() {
         List<Particle> particlesList = getPlanetList();
         particlesList.add(new Particle(new Vector(VENUS_X, VENUS_Y), new Vector(VENUS_VX, VENUS_VY), 30, VENUS_MASS));
-        particlesList.add(new Particle(new Vector(JUPITER_X, JUPITER_Y), new Vector(JUPITER_VX, JUPITER_VY), 30, JUPITER_MASS));
+        particlesList.add(new Particle(new Vector(MARS_X * 3, MARS_Y * 3), new Vector(JUPITER_VX, JUPITER_VY), 30, JUPITER_MASS));
         particlesList.add(new Particle(new Vector(MERCURY_X, MERCURY_Y), new Vector(MERCURY_VX, MERCURY_VY), 30, MERCURY_MASS));
         particlesList.add(new Particle(new Vector(MOON_X, MOON_Y), new Vector(MOON_VX, MOON_VY), 30, MOON_MASS));
         return particlesList;
